d966301748f32cd4910ebe179146f50634388f75
StormrageXC <610211409@qq.com>
2022-08-09 08:17:44 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # Express
-Express and Connect
+## express-generator
+### 安装
+
+> npx express-generator
+
+或者
+
+> npm install -g express-generator
+### 命令行
+>             
+>   express -h
+> 
+>   Usage: express [options] [dir]
+> 
+>   Options:
+> 
+>   -h, --help          输出使用方法
+>
+>   --version       输出版本号
+>
+>   -e, --ejs           添加对 ejs 模板引擎的支持
+>
+>   --hbs           添加对 handlebars 模板引擎的支持
+>
+>   --pug           添加对 pug 模板引擎的支持
+>
+>   -H, --hogan         添加对 hogan.js 模板引擎的支持
+>
+>   --no-view       创建不带视图引擎的项目
+>
+>   -v, --view <engine> 添加对视图引擎（view） <engine> 的支持 (ejs|hbs|hjs|jade|pug|twig|vash) （默认是 jade 模板引擎）
+>
+>   -c, --css <engine>  添加样式表引擎 <engine> 的支持 (less|stylus|compass|sass) （默认是普通的 css 文件）
+>
+>   --git           添加 .gitignore
+>
+>   -f, --force         强制在非空目录下创建
+>  
+### 托管静态文件
+如果要使用多个静态资源目录，请多次调用 express.static 中间件函数：
+> app.use(express.static('public'))
+>
+> app.use(express.static('files'))
--- app.js
+++ app.js
@@ -3,71 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-const entries = require('./routes/entries');
-const register = require('./routes/register');
-const login = require('./routes/login');
-const validate = require('./middleware/validate');
-const message = require('./middleware/message');
-const user = require('./middleware/user');
-const api = require('./routes/api');
-const page = require('./middleware/page');
-const Entry = require('./models/entry');
-
 
 var app = express();
-app.locals.email = 'me@myapp.com'
-    // view engine setup
-    // app.set('views', path.join(__dirname, 'views'));
-console.log(app.get('views'));
-app.set('view engine', 'ejs');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: true
-}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(user);
-app.use(message);
 
-
-app.get('/', entries.list);
-app.get('/post', entries.form);
-app.post('/post', validate.require('entry[title]'), validate.lengthAbove('entry[title]', 4), entries.submit);
-
-app.get('/register', register.form);
-app.post('/register', register.submit);
-
-app.get('/login', login.form);
-app.post('/login', login.submit);
-app.get('/logout', login.logout);
-
-app.get('/api/user/:id', api.user);
-app.post('/api/entry', entries.submit);
-app.get('/api/entries/:page?', page(Entry.count), api.entries);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
